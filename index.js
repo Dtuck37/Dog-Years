efe88c41a0fda144165c9290dfeccd438f46d7ff
Dtuck37 <104952981+Dtuck37@users.noreply.github.com>
2022-06-10 10:35:07 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let myAge = 31, 
+    earlyYears = 2,
+    b = earlyYears;
+
+    earlyYears = b * 10.5
+
+let laterYears = myAge - 2;
+
+laterYears = laterYears * 4;
+
+let myAgeInDogYears = myAge + laterYears;
+
+let myName = "Derick L Tucker Jr.".toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
